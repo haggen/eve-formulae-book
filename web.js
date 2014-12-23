6b8034ac30c7b9a254e11e8506d74e84ef8e9308
Arthur Corenzan <arthur@corenzan.com>
2014-12-23 12:53:07 -0200

    fix nested array when querying, add discoverability

--- web.js
+++ web.js
@@ -13,6 +13,28 @@ var database = {
   systems: require('./data/systems'),
 };
 
+app.get('/', function(request, response) {
+  var base = request.protocol + '://' + request.hostname + '/';
+
+  response.json({
+    collections: {
+      regions: base + 'regions.json',
+      regions: base + 'types.json',
+      regions: base + 'systems.json',
+    },
+    querystring: {
+      id: 'integer, find by id',
+      name: 'string, find by exact name',
+      match: 'string, match name by regular expression, case insensitive'
+    },
+    examples: [
+      base + '/regions.json?name=Sinq%20Laison',
+      base + '/systems.json?id=30042715',
+      base + '/types.json?match=miner',
+    ]
+  });
+};
+
 app.get('/:collection.json', function(request, response) {
   if('name' in request.query) {
     response.json([database[request.params.collection].find(function(item) {
@@ -22,10 +44,10 @@ app.get('/:collection.json', function(request, response) {
     response.json([database[request.params.collection].find(function(item) {
       return item.id == request.query.id;
     })]);
-  } else if('q' in request.query) {
-    response.json([database[request.params.collection].filter(function(item) {
+  } else if('match' in request.query) {
+    response.json(database[request.params.collection].filter(function(item) {
       return item.name.match(new RegExp(request.query.q, 'i'));
-    })]);
+    }));
   } else {
     response.json(database[request.params.collection]);
   }
